96eb054a5d250f2af7e3e30b4ae9f4848a789c46
Michael Benfield <mike.benfield@gmail.com>
2018-07-06 14:44:53 -0700

    remove copyright headers

--- redwood/src/data.rs
+++ redwood/src/data.rs
@@ -1,11 +1,3 @@
-// Copyright 2017 Michael Benfield
-// This file is part of Redwood. You may distribute and/or modify Redwood
-// under the terms of either the MIT license or the Apache license (version
-// 2.0), at your option. You should have received a copy of these licenses along
-// with Redwood (see the files LICENSE-MIT and LICENSE-APACHE). If not, see
-// <http://opensource.org/licenses/MIT> and
-// <http://www.apache.org/licenses/LICENSE-2.0>.
-
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::mem;
--- redwood/src/lib.rs
+++ redwood/src/lib.rs
@@ -1,11 +1,3 @@
-// Copyright 2017 Michael Benfield
-// This file is part of Redwood. You may distribute and/or modify Redwood
-// under the terms of either the MIT license or the Apache license (version
-// 2.0), at your option. You should have received a copy of these licenses along
-// with Redwood (see the files LICENSE-MIT and LICENSE-APACHE). If not, see
-// <http://opensource.org/licenses/MIT> and
-// <http://www.apache.org/licenses/LICENSE-2.0>.
-
 extern crate crossbeam_utils;
 #[macro_use]
 extern crate failure;
